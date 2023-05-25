c57e971ca1a253baa7e149865de6cc57c80accbb
Kinshuk Phalke <kp185203@ncr.com>
2023-05-25 11:08:19 -0400

    updated image src link

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ['retaildemo.ncrcloud.com', 'https://via.placeholder.com', 'via.placeholder.com', 'www.google.com', 'encrypted-tbn0.gstatic.com']
+    domains: ['retaildemo.ncrcloud.com', 'https://via.placeholder.com', 'via.placeholder.com', 'www.google.com', 'encrypted-tbn0.gstatic.com', 'media.istockphoto.com']
   },
   eslint: {
     ignoreDuringBuilds: true
